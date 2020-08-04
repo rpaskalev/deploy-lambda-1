b70dcd53ed7aef0500c92135b433fa35ef4e3145
amitsehgal <amitsehgal.us@gmail.com>
2020-08-03 21:10:38 -0400

    changing the output of lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,5 @@
 import json
 
-
 def lambda_handler(event, context):
     print(json.dumps(event))
     return {
@@ -8,5 +7,5 @@ def lambda_handler(event, context):
         'headers': {
             'Content-Type': 'application/json'
         },
-        'body': json.dumps({'message': 'Hello-OK'})
+        'body': json.dumps({'message': 'OK-Tested'})
     }
